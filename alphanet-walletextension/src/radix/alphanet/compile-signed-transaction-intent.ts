cb587c50024557944e6f24bf66d28ee51efc920d
0xOmarA <OmarAbdulla7@hotmail.com>
2022-10-04 03:41:10 +0300

    Update for cost unit changes

--- alphanet-walletextension/src/radix/alphanet/compile-signed-transaction-intent.ts
+++ alphanet-walletextension/src/radix/alphanet/compile-signed-transaction-intent.ts
@@ -10,11 +10,11 @@ export const compileSignedTransactionIntent = (
     .andThen((transactionService) => {
       const compiledResult = transactionService.compileSignedTransactionIntent(
         signedTransactionIntent
-      );
+      ); 
 
       return "error" in compiledResult
         ? errAsync(compiledResult)
         : okAsync(compiledResult);
     })
     .map(({ compiled_signed_intent }) => compiled_signed_intent)
-    .mapErr(errorIdentity("Could not compile transaction intent"));
+    .mapErr(errorIdentity("Could not compile signed transaction intent"));
