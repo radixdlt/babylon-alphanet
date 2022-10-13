fee61e64ca4c564fa7a1b6203cbc9ddf54bbd046
Alex Stelea <alex.s@rdx.works>
2022-10-13 13:06:00 +0200

    update esm build filename

--- gateway-api-v0/typescript-sdk/vite.config.js
+++ gateway-api-v0/typescript-sdk/vite.config.js
@@ -7,7 +7,6 @@ module.exports = defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'lib/index.ts'),
       name: 'alphanet-gateway-api-v0-sdk',
-      fileName: (format) => `alphanet-gateway-api-v0-sdk.${format}.js`,
     },
   },
 })
